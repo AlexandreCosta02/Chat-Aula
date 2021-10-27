2b158cb4729d0cc7d0e078aa226ebe519ed9768e
AlexandreCosta02 <xandycosta.ac02@gmail.com>
2021-10-26 21:20:23 -0300

    Alteração Aula 26/10/21

--- chat.php
+++ chat.php
@@ -1,44 +1,65 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Chat</title>
-		<link rel="stylesheet" href="Css/chat.css">
-	</head>
-	<body>
-		<?php
-			session_start();
-			if (!isset($_SESSION['nick'])){
-				header('location: Index.php');
-			}
-			$conexao = new mysqli("localhost","root","usbw","chat");
-		?>
-		<div class="chat">
-			<?php
-				$sql = 'SELECT origem, mensagem FROM msg';
-				$resultado = $conexao->query($sql);
-				while ($msg = $resultado->fetch_array()){
-					$cor = ($msg['origem'] == $_SESSION['nick']) ? 'meu' : '';
-					echo '<b class="'.$cor.'">';
-					echo $msg['origem'].":  ".$msg['mensagem'];
-					echo "<br>";
-				}
-			?>
-		</div>
-		<form method="POST">
-			Mensagem:
-			<textarea name="msg"></textarea>
-			<button>Enviar</button>
-		</form>
-		<?php
-			if ($_POST){
-				$sql = 'INSERT INTO msg VALUES (null,"'.$_SESSION['nick'].'","'.$_POST['msg'].'")';
-				$resultado = $conexao->query($sql);
-				if (!$resultado){
-					echo "Mensagem não enviada: ".$conexao->error;
-				}
-			}
-		?>
-	</body>
-</html>
+<?php
+session_start();
+if(!isset($_SESSION['nick'])){
+	header('location: index.php');
+}
+$conexao = new mysqli("localhost","root","usbw","chat");
 
+?>
+<style>
+	.chat{
+		width: 80vw;
+		height: 70vh;
+		margin-top: 5vh;
+		margin-left: 10vw;
+		border:1px solid red;
+	}
+	.meu{
+		color:red;
+	}
+</style>
+<script src="jquery-3.6.0.min.js"></script>
+<script>
+	function buscarmsg(){
+		$.ajax({
+			url:'msg.php',
+			data: null,
+			type: 'GET',
+			success: function(retorno){
+				$('.chat').html(retorno);
+			}
+		});
+	}
+
+	setInterval(function(){
+		buscarmsg();
+	},2000);
+
+	$(document).on('submit','#enviar',function(e){
+		$.ajax({
+			url: 'msg.php',
+			data: $(this).serialize(),
+			type: 'POST',
+			success:function(){
+				$('#msg').val('');
+			}
+		});
+		e.preventDefault();
+	});
+
+</script>
+<div class="chat">	
+	<?php
+	
+
+	?>
+</div>
+<form id="enviar">
+	
+	Mensagem:
+	<textarea name="msg" id="msg"></textarea>
+	<button>Enviar</button>
+</form>
+<?php
+
+?>
