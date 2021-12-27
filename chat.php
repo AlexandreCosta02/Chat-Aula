3792c9f306c7a6b3b4d0be58cb3a9154b86bfd46
AlexandreCosta02 <xandycosta.ac02@gmail.com>
2021-12-27 13:40:14 -0300

    alterações 27/12/2021

--- chat.php
+++ chat.php
@@ -1,3 +1,10 @@
+<?php
+		session_start();
+		if(!isset($_SESSION['nick'])){
+			header('location: index.php');
+		}
+		$conexao = new mysqli("localhost","eulzfuxx_root","152634152634","eulzfuxx_LOGIN");
+	?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -9,55 +16,57 @@
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	</head>
 	<body>
-	<?php
-		session_start();
-		if(!isset($_SESSION['nick'])){
-			header('location: index.php');
-		}
-		$conexao = new mysqli("localhost","eulzfuxx_root","152634152634","eulzfuxx_LOGIN");
-	?>
-	<script src="jquery-3.6.0.min.js"></script>
-	<script>
-		function buscarmsg(){
-			$.ajax({
-				url:'msg.php',
-				data: null,
-				type: 'GET',
-				success: function(retorno){
-					$('.chat').html(retorno);
-				}
+		<div id="barra" class="row">
+			<div class="col"></div>
+			<div id="titulo" class="col">
+				<h1>Chat Etec</h1>
+			</div>
+			<div class="col">
+				<a id="sair" href="index.php" class="fas fa-sign-out-alt"></a>
+			</div>
+		</div>
+		<script src="jquery-3.6.0.min.js"></script>
+		<script>
+			function buscarmsg(){
+				$.ajax({
+					url:'msg.php',
+					data: null,
+					type: 'GET',
+					success: function(retorno){
+						$('.chat').html(retorno);
+					}
+				});
+			}
+
+			setInterval(function(){
+				buscarmsg();
+			},2000);
+
+			$(document).on('submit','#enviar',function(e){
+				$.ajax({
+					url: 'msg.php',
+					data: $(this).serialize(),
+					type: 'POST',
+					success:function(){
+						$('#msg').val('');
+					}
+				});
+				e.preventDefault();
 			});
-		}
 
-		setInterval(function(){
-			buscarmsg();
-		},2000);
-
-		$(document).on('submit','#enviar',function(e){
-			$.ajax({
-				url: 'msg.php',
-				data: $(this).serialize(),
-				type: 'POST',
-				success:function(){
-					$('#msg').val('');
-				}
-			});
-			e.preventDefault();
-		});
-
-	</script>
-	<div class="chat">	
+		</script>
+		<div class="chat">	
+			<?php
+			
+			?>
+		</div>
+		<form id="enviar">
+			<textarea name="msg" id="msg"></textarea>
+			<br>
+			<button id="btnEnviar" class="fas fa-check"></button>
+		</form>
 		<?php
-		
-		?>
-	</div>
-	<form id="enviar">
-		<textarea name="msg" id="msg"></textarea>
-		<br>
-		<button id="btnEnviar" class="fas fa-check"></button>
-	</form>
-	<?php
 
-	?>
+		?>
 	</body>
 </html>
